02601dea7dfe37ffc7a2fe2e265eb43c4e463fa3
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2020-03-20 23:52:39 +0100

    Fix mixed up key and value

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import {useLocation} from 'react-router-dom'
 export function useQuery(): { [key: string]: string } {
     const params = new URLSearchParams(useLocation().search);
     const query: { [key: string]: string } = {};
-    params.forEach((key, value) => query[key] = value)
+    params.forEach((value, key) => query[key] = value)
     return query;
 }
